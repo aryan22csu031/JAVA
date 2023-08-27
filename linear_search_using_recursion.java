ae006328399a013343777d55a400ee51642f991f
Aryan Arora <141122872+aryan22csu031@users.noreply.github.com>
2023-08-27 10:00:33 +0530

    Update linear_search_using_recursion.java

--- linear_search_using_recursion.java
+++ linear_search_using_recursion.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class ls{
     static boolean isExist(int[] arr,int target,int index){
         if(arr[index] == target){
@@ -12,9 +15,21 @@ class ls{
 
         return arr[index] == target || isExist(arr,target,index+1);
     }
+    static ArrayList isExistMultiple(int[] arr,int target, int index, ArrayList<Integer> list){
+        if(index==arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return isExistMultiple(arr,target,index+1,list);
+    }
     public static void main(String args[]){
+        ArrayList<Integer> array = new ArrayList<>();
         int[] arr1 = {4,8,2,6,1};
         System.out.println(isExist(arr1,1,0));
+        int[] arr2 = {4,8,2,6,1,8,4};
+        System.out.println(isExistMultiple(arr2,4,0,array));
 
     }
 }
