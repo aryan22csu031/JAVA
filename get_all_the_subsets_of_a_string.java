911fe0ed6fcc36f060aad99a1b73079e338aad7c
Aryan Arora <141122872+aryan22csu031@users.noreply.github.com>
2023-09-07 13:44:54 +0530

    Update get_all_the_subsets_of_a_string.java

--- get_all_the_subsets_of_a_string.java
+++ get_all_the_subsets_of_a_string.java
@@ -3,6 +3,7 @@ class strings2{
     public static void main(String[] args) {
         String str = "abc";
         makeSubsets("",str);
+        System.out.print(subSetArray("","abc"));
     }
     static void makeSubsets(String p, String up){
         if (up.isEmpty()) {
@@ -12,5 +13,17 @@ class strings2{
         char ch = up.charAt(0);
         makeSubsets(p+ch,up.substring(1));
         makeSubsets(p,up.substring(1));
+    }
+    static ArrayList<String> subSetArray(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> arr = new ArrayList<>();
+            arr.add(p);
+            return arr;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> left = subSetArray(p+ch,up.substring(1));
+        ArrayList<String> right = subSetArray(p,up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
