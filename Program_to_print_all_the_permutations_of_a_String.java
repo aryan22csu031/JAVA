9cf8c3c9f4c7cb8ab4be97efc9316c2193328cae
Aryan Arora <141122872+aryan22csu031@users.noreply.github.com>
2023-09-08 19:50:23 +0530

    Update Program_to_print_all_the_permutations_of_a_String.java

--- Program_to_print_all_the_permutations_of_a_String.java
+++ Program_to_print_all_the_permutations_of_a_String.java
@@ -1,8 +1,12 @@
 package StringsInJava;
+
+import java.util.ArrayList;
+
 class perm{
     public static void main(String[] args) {
         String a = "abc";
-        permutations("","abc");
+        permutations("",a);
+        System.out.print(permutationList("",a));
     }
     static void permutations(String p, String up){
         if(up.isEmpty()){
@@ -15,5 +19,20 @@ class perm{
             String SecondPos = p.substring(i);
             permutations(FirstPos + ch+ SecondPos,up.substring(1));
         }
+    }
+    static ArrayList<String> permutationList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> l = new ArrayList<>();
+            l.add(p);
+            return l;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = up.charAt(0);
+        for(int i=0; i <= p.length(); i++){
+            String FirstPos = p.substring(0,i);
+            String SecondPos = p.substring(i);
+            ans.addAll(permutationList(FirstPos + ch+ SecondPos,up.substring(1)));
+        }
+        return ans;
     }
 }
